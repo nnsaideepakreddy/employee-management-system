1f7a6401a87e52f88d969ad802de6e149da2ad86
Sai Deepak Reddy Nareddy Narayanareddy <159862468+nnsaideepakreddy@users.noreply.github.com>
2024-12-05 18:56:35 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,20 +2,55 @@ import streamlit as st
 import pandas as pd
 import pg8000
 
+import ssl
+
 def create_db_connection():
+    # Hardcoded credentials
     host = "pg-205cff4d-nnsaideepak-adde.g.aivencloud.com"
     database = "defaultdb"
     user = "avnadmin"
     password = "AVNS_sPl-2cVqG3XjT3h_orr"
     port = 21517
+
+    # The CA certificate as a string
+    ca_certificate = """-----BEGIN CERTIFICATE-----
+MIIEQTCCAqmgAwIBAgIUEyelzZlEvJX6z55ri6q3F6Fla/EwDQYJKoZIhvcNAQEM
+BQAwOjE4MDYGA1UEAwwvYzhlOTZmMTMtMjY3Yy00ODFhLTg1M2QtYjM1OTQ2ODA3
+YzQzIFByb2plY3QgQ0EwHhcNMjQxMjA1MjIzNjM5WhcNMzQxMjAzMjIzNjM5WjA6
+MTgwNgYDVQQDDC9jOGU5NmYxMy0yNjdjLTQ4MWEtODUzZC1iMzU5NDY4MDdjNDMg
+UHJvamVjdCBDQTCCAaIwDQYJKoZIhvcNAQEBBQADggGPADCCAYoCggGBALg434sD
+GXChgLR/xS8RxtgFlpJgrDxl3eeDkFHioWU+os8oi67XwimNjPJIhzztRXf6CLnP
+/todadn1NoS0UbBB2ppXeISABmvjbxnNUmmA22B77alN1ctQxIcnq0sDq8QISp7c
+R882/qXzB5WbVgCMqFZhaysurk+2zcoiZiHzkZc7kic9vyMsjPdJNxE30fPM5FSN
+ihrMb5lgp9dLwIw1swD+BNsJ+ckTM853kDE/zgzH8vn3mCoWwqWYOLK2E3y5PK7+
+/++2ER48gn+f/s44342sixa//1opodCr+7f77DKgrFMPDM1tgCSvLfjL5/cNb1nP
+t8dhMRk2MLCkIBH/+SEKh4FGf2NZLL+tD1fEyMz0zqxmdhXn6FUe84b2IbWtncik
+PXudDnidhlzv0c5NEnGgUZ8y5zRvdk21XiaXD0BoNV28cEriYG8I9M1Dnw34fnan
+vaxyGD5lQXqTB0OxZyzuLLWXmiq6ch9rHRExZqRCRaf2ejdOea9nz0vF/QIDAQAB
+oz8wPTAdBgNVHQ4EFgQUjjodE2cOBic9yOraxs/bG1Yg0U0wDwYDVR0TBAgwBgEB
+/wIBADALBgNVHQ8EBAMCAQYwDQYJKoZIhvcNAQEMBQADggGBAD1K7IFK3VGbCE6R
+SPlCBJVKXhS0p2MLh58Llz/mO8R328ieqJae7XYahsjBi0dWjEkb37cjox/K+eXA
+SzPOS0jFIC+4VKm5ecFsE//N+EKazhSJEqcLbUvcKYBiA5FD5E8Zw0riRirXzbDs
+YoD+b3PuBx7dMJr4moZNtshKLwAh+WJAC35YHtfl+V/EvXQKV4D7DO+pmIb/b9g8
+K5D04P2knQI6v0J3fOyj9kscnrvDu9m7m8mMZwqjuZ30JLdXKRZdwug7ciKgcia1
+eS8W45EJZdbIrLarKes/hpW88mjQXy1JnvSHkrYyS74UDnIW1bRDjDrGeUTOh3JK
+LVK+MFwlUxL4PexOoqza6+XxvJq5yDkp9QXi8o88LwUM31cqSMpFp/Wjs6LJpP2g
+lWEhGeAhX5sqspVZL89X/YdHSt8NxOezTna678e9OZI1JV/PEEXsnK/Jauagl4t/
+4CgiT9LtV2+eZewM1m/3uZ8yjy8Jfj3lNKzkdXZKJgEkRxQu9g==
+-----END CERTIFICATE-----"""
+
     try:
+        # Create an SSL context and load the CA certificate from memory
+        sslcontext = ssl.create_default_context()
+        sslcontext.load_verify_locations(cadata=ca_certificate)
+        
         conn = pg8000.connect(
             host=host,
             database=database,
             user=user,
             password=password,
             port=port,
-            ssl_context=True
+            ssl_context=sslcontext
         )
         return conn
     except Exception as e:
